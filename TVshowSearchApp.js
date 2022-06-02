977243a704ec6bc214cb2de192530f3d68919b50
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2022-06-01 21:10:31 -0400

    tvshows-search-app

---
+++ TVshowSearchApp.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
+const reset = document.querySelector("#reset");
+const deleteImgs = function () {
+  const imgs = document.querySelectorAll("img");
+  for (let img of imgs) {
+    img.remove();
+  }
+};
+reset.addEventListener("click", deleteImgs);
